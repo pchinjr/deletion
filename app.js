0e1087bd9777087b3515ef3fcb6d418da6a3d896
Paul Chin Jr <pchinjr@gmail.com>
2015-12-02 05:35:44 +0000

    used stop function

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ $('#content').on('keyup', function(){
   var splitt = entry.split("");
   characters = splitt;
   removeTimer.play();
+  console.log("play")
 });
 
 $('#content').on('keypress', function(){
-  removeTimer.reset();
+  removeTimer.stop();
+  console.log("stop");
 });
 
 var removeTimer = $.timer( function() {
